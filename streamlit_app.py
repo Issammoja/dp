fec52e51669b3dc1168296871cf62fad639538dc
Issa Likhiti <121372176+Issammoja@users.noreply.github.com>
2025-03-02 12:42:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,7 @@ import pandas as pd
 st.title('machine learning app')
 
 st.info('This app builds a machine learning model')
+with st.expander('Data'):
+  st.write('**Raw data**')
 df= pd.read_csv('https://raw.githubusercontent.com/Issammoja/dp/refs/heads/master/kenya-climate-data-1991-2016-rainfallmm.csv')
 df
