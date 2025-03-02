34c91369cbe0b18f091a367b7ff0d0b0e6c4e3c9
Issa Likhiti <121372176+Issammoja@users.noreply.github.com>
2025-03-02 12:51:28 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,5 +8,7 @@ st.title('Early Drought Warning System')
 st.info('This app builds a predictive model using regression model')
 with st.expander('Data'):
   st.write('**Raw data**')
-df= pd.read_csv('https://raw.githubusercontent.com/Issammoja/dp/refs/heads/master/kenya-climate-data-1991-2016-rainfallmm.csv')
-df
+  df= pd.read_csv('https://raw.githubusercontent.com/Issammoja/dp/refs/heads/master/kenya-climate-data-1991-2016-rainfallmm.csv')
+  df
+  st.write('**X**')
+  x = df.drop('
