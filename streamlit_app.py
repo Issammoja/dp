41279af0c475ba0c0409b6cd489a47576942765c
Issa Likhiti <121372176+Issammoja@users.noreply.github.com>
2025-03-03 10:28:14 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ with st.expander('Data'):
 
 with st.expander('Data visualization'):
 
-  st.scatter_chart(data=df, x='Year', y='Rainfall - (MM)')
+  st.st.bar_chart(data=df, x='Year', y='Rainfall - (MM)')
 
 
   
